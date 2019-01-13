821c56d6541644010e1601b2ee15d2e171d11261
Mattia <tia_ben@tin.it>
2019-01-13 16:13:22 +0100

    Correct Glad premake5.lua identations.

--- Hazel/vendor/Glad/premake5.lua
+++ Hazel/vendor/Glad/premake5.lua
@@ -8,14 +8,14 @@ project "Glad"
     files
     {
         "include/glad/glad.h",
-		"include/KHR/khrplatform.h",
-		"src/glad.c"
+        "include/KHR/khrplatform.h",
+        "src/glad.c"
     }
 
-	includedirs
-	{
-		"include"
-	}
+    includedirs
+    {
+        "include"
+    }
 
     filter "system:windows"
         systemversion "latest"
