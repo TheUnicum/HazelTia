249852ebbe92bf7f43ba468f40b0142e91950bdb
TheUnicum <tia_ben@tin.it>
2020-05-21 16:23:55 +0200

    [46-47] Camera Controllers - Game Engine series. + UPDATE to VS2019 (new premake)

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 :PROMPT
-SET /P AREYOUSURE=Do you want regenerate VS2017 Solution Project? Are you sure(Y/[N])?
+SET /P AREYOUSURE=Do you want regenerate VS2019 Solution Project? Are you sure(Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-call vendor\bin\premake\premake5.exe vs2017
+call vendor\bin\premake\premake5.exe vs2019
 PAUSE
 :END
 endlocal
