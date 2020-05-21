6e4f5c190e64c328d632b7c500d01001fc33dcf6
TheUnicum <tia_ben@tin.it>
2020-05-21 17:42:26 +0200

    <PR_Old> Grouped dependencies.

--- scripts/Win-GenProjects.bat
+++ scripts/Win-GenProjects.bat
@@ -4,7 +4,10 @@ setlocal
 SET /P AREYOUSURE=Do you want regenerate VS2019 Solution Project? Are you sure(Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
+pushd ..\
 call vendor\bin\premake\premake5.exe vs2019
+popd
+
 PAUSE
 :END
 endlocal
