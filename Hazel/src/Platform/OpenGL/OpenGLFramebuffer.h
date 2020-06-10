8e1f4a4cf50ad8dec03fb92b75dd1ccf9a15d399
TheUnicum <tia_ben@tin.it>
2020-06-10 20:32:52 +0200

    [72] <Added basic Framebuffer class> Framebuffers | Game Engine series.

---
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Hazel/Renderer/Framebuffer.h"
+
+namespace Hazel{
+
+	class OpenGLFrameBuffer : public Framebuffer
+	{
+	public:
+		OpenGLFrameBuffer(const FramebufferSpecification& spec);
+		virtual ~OpenGLFrameBuffer();
+
+		void Invalidate();
+
+		virtual void Bind() override;
+		virtual void Unbind() override;
+
+		virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+
+		virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; };
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_ColorAttachment, m_DepthAttachment;
+		FramebufferSpecification m_Specification;
+	};
+
+}
