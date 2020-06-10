430fb98a7e66885e05abc7e1ece240eb55c2e9a5
TheUnicum <tia_ben@tin.it>
2020-06-10 17:57:05 +0200

    [71] (Skipped 65-66-67-68-69) Framebuffers | Game Engine series.

--- Hazel/src/Platform/OpenGL/OpenGLTexture.h
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.h
@@ -15,6 +15,7 @@ namespace Hazel {
 
 		virtual uint32_t GetWidth() const override { return m_Widht; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
+		virtual uint32_t GetRendererID() const { return m_RendererID; }
 
 		virtual void SetData(void* data, uint32_t size) override;
 
